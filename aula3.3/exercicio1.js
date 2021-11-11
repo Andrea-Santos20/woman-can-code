2a398be15ef0b88e16377b95ce0068a7d8397351
Andrea Santos <dea0312@gmail.com>
2021-11-11 06:50:47 -0300

    exercicios - parte3

---
+++ aula3.3/exercicio1.js
@@ -0,0 +1,8 @@
+var vaiChover = true;
+if (vaiChover) {
+  console.log("Leve seu guarda-chuva, hoje vai chover.")
+} else {
+	console.log("Fique tranquilo, hoje será um dia se sol.")
+}
+
+console.log(vaiChover)
---
+++ aula3.3/exercicio2.js
@@ -0,0 +1,26 @@
+var num = 5;
+var saldo = 2000000;
+var fruta = "Banana";
+var valor = "50";
+var locomocao = "Carro"; 
+var dinheiro = 50; 
+var salario = 975;
+
+console.log(num > 1) 
+console.log(saldo < 3000000000)
+console.log(fruta == "Banana") 
+console.log(valor === 50) 
+console.log(locomocao != "bicicleta") 
+console.log(dinheiro > 27)
+console.log (salario <= 1200)
+
+
+/*
+
+console.log(fruta)
+console.log(valor)
+console.log(locomocao)
+console.lo(dinheiro)
+console.log(salario)
+*/
+
