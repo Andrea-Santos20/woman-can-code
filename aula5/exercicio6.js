d47f4f5bbf7cd0c0ab0f6371cde00b74e9f73d79
Andrea Santos <dea0312@gmail.com>
2021-11-15 11:59:39 -0300

    exercicios-parte2

--- aula5/exercicio6.js
+++ aula5/exercicio6.js
@@ -1,13 +1,16 @@
-    const valoresNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    function criaPares(valoresNum){
     
-        for (let criaPares = 0; criaPares < valoresNum.length; criaPares++) {
-            if (criaPares % 2 == 0) {
+    
+        for (let cont = 0; cont < valoresNum.length; cont++) {
+            if (valoresNum[cont] % 2 === 0) {
 
-             console.log(valoresNum[criaPares]);
+             console.log(valoresNum[cont]);
 
             }
-               
+        }
        }
+       const valoresNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+       criaPares(valoresNum);
              
     
    
